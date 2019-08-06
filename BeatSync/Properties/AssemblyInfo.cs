e9dd93d64ef46ab1ef9c49f86823d31a64eb4105
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-08-05 23:29:06 -0500

    .

--- BeatSync/Properties/AssemblyInfo.cs
+++ BeatSync/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BeatSync")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Zingabopp 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
