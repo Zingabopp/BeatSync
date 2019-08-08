f682a55ae84de5e1dc7d70f21c7fff3218c559e2
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-08-08 02:36:17 -0500

    Submodule update, started FloatingText class

---
+++ BeatSync/UI/FloatingText.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using TMPro;
+using CustomUI.BeatSaber;
+
+namespace BeatSync.UI
+{
+    public class FloatingText : MonoBehaviour
+    {
+        public static TMP_FontAsset TMP_Font;
+        private const string FontName = "Teko-Medium SDF No Glow";
+        private TextMeshProUGUI textMesh;
+
+        private string _displayedText;
+        public string DisplayedText
+        {
+            get { return _displayedText ?? ""; }
+            set
+            {
+                if(_displayedText != value)
+                {
+                    _displayedText = value;
+                    if (textMesh != null)
+                        textMesh.text = value;
+                }
+            }
+        }
+
+
+        public void Awake()
+        {
+            StartCoroutine(Utilities.WaitForResource<TMP_FontAsset>(FontName, font =>
+            {
+                TMP_Font = UnityEngine.Object.Instantiate<TMP_FontAsset>(font);
+                CreateText();
+            }));
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void CreateText()
+        {
+            Logger.log.Debug("Font found, creating text.");
+            GameObject gameObject = new GameObject();
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
+            gameObject.transform.position = new Vector3(0f, 0f, 2.5f);
+            gameObject.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            gameObject.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            Canvas canvas = gameObject.AddComponent<Canvas>();
+            canvas.renderMode = UnityEngine.RenderMode.WorldSpace;
+            (canvas.transform as RectTransform).sizeDelta = new Vector2(200f, 50f);
+            TextMeshProUGUI textMeshProUGUI = BeatSaberUI.CreateText(canvas.transform as RectTransform, _displayedText, new Vector2(0f, -20f), new Vector2(400f, 20f));
+            textMeshProUGUI.text = _displayedText;
+            textMeshProUGUI.fontSize = 10f;
+            textMeshProUGUI.alignment = TextAlignmentOptions.Center;
+            Logger.log.Debug("Text created.");
+        }
+
+
+    }
+}
---
+++ BeatSync/Utilities.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using TMPro;
+
+namespace BeatSync
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Attempts to find a resource of type TResource with the given name. An action can be provided to execute when the object is found.
+        /// pollRateMillis is the interval in milliseconds to check for the existance of the object.
+        /// </summary>
+        /// <typeparam name="TResource"></typeparam>
+        /// <param name="name"></param>
+        /// <param name="action"></param>
+        /// <param name="pollRateMillis"></param>
+        /// <returns></returns>
+        public static IEnumerator<WaitForSeconds> WaitForResource<TResource>(string name, Action<TResource> action = null, int pollRateMillis = 100)
+            where TResource : UnityEngine.Object
+        {
+            Func<bool> waitFunc = () => Resources.FindObjectsOfTypeAll<TResource>().Any(o =>
+            {
+                if (o.name != name)
+                    return false;
+                try
+                {
+                    action?.Invoke(o);
+                }
+                catch (Exception ex)
+                {
+                    Logger.log.Error($"Error invoking action for WaitForResource<{typeof(TResource)}> with name {name}.\n{ex?.Message}\n{ex?.StackTrace}");
+                }
+                return true;
+            });
+            var wait = new WaitForSeconds(Math.Max(pollRateMillis / 1000f, .02f));
+            while (!waitFunc.Invoke())
+            {
+                yield return wait;
+            }
+            //yield return waitFunc;
+
+        }
+    }
+}
