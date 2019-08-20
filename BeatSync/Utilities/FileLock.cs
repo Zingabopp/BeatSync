44210b934ec2a201f88a8865e9070f10f56811ab
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-08-20 13:43:02 -0500

    Added FileLock class

---
+++ BeatSync/Utilities/FileLock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatSync.Utilities
+{
+    /// <summary>
+    /// Use to make sure multiple things aren't writing to the same file.
+    /// </summary>
+    public class FileLock
+    {
+
+    }
+}
