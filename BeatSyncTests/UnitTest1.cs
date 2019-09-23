4390025a7383bda3df2e20c137c2169773ca54fe
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-09-23 12:46:43 -0500

    MSBuild experiments

--- BeatSyncTests/UnitTest1.cs
+++ BeatSyncTests/UnitTest1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BeatSync;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace BeatSyncTests
 {
@@ -11,6 +14,16 @@ namespace BeatSyncTests
         public void TestMethod1()
         {
             Console.WriteLine("Test");
+        }
+
+        [TestMethod]
+        public void GetStuff()
+        {
+            JToken thing = JsonConvert.DeserializeObject<JToken>(File.ReadAllText("manifest.json"));
+            string gameVersion = thing["gameVersion"].Value<string>();
+            string version = thing["version"].Value<string>();
+            Assert.AreEqual("1.3.0", gameVersion);
+            Assert.AreEqual("0.2.4", version);
         }
     }
 }
