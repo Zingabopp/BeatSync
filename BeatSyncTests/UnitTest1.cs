b41699c79a9da95fc65997210ccadc74f041f599
Zingabopp <zingabopp@gmail.com>
2019-09-25 21:04:14 -0500

    Added check for matching plugin version and AssemblyVersion

--- BeatSyncTests/UnitTest1.cs
+++ BeatSyncTests/UnitTest1.cs
@@ -4,6 +4,8 @@ using BeatSync;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
 
 namespace BeatSyncTests
 {
@@ -16,14 +18,54 @@ namespace BeatSyncTests
             Console.WriteLine("Test");
         }
 
-        [TestMethod]
-        public void GetStuff()
-        {
-            JToken thing = JsonConvert.DeserializeObject<JToken>(File.ReadAllText("manifest.json"));
-            string gameVersion = thing["gameVersion"].Value<string>();
-            string version = thing["version"].Value<string>();
-            Assert.AreEqual("1.3.0", gameVersion);
-            Assert.AreEqual("0.2.4", version);
-        }
+        //[TestMethod]
+        //public void GetStuff()
+        //{
+        //    string PluginVersion;
+        //    string GameVersion;
+
+        //    try
+        //    {
+        //        string manifestFile = @"manifest.json";
+        //        string assemblyFile = @"Properties\AssemblyInfo.cs";
+        //        var assemblyVersionRegex = new Regex(@"^\[assembly: AssemblyVersion\(\""?(.*)\""\)\]",
+        //            RegexOptions.Multiline | RegexOptions.Compiled);
+        //        if (!File.Exists(manifestFile))
+        //        {
+        //            throw new FileNotFoundException("Could not find manifest: " + Path.GetFullPath(manifestFile));
+        //        }
+        //        if (!File.Exists(assemblyFile))
+        //        {
+        //            throw new FileNotFoundException("Could not find AssemblyInfo: " + Path.GetFullPath(assemblyFile));
+        //        }
+        //        JToken manifestJson = JsonConvert.DeserializeObject<JToken>(File.ReadAllText(manifestFile));
+        //        if (manifestJson["version"] != null)
+        //        {
+        //            string version = manifestJson["version"].Value<string>();
+        //            PluginVersion = string.IsNullOrEmpty(version) ? "E.R.R" : version;
+        //        }
+        //        if (manifestJson["gameVersion"] != null)
+        //        {
+        //            string gameVersion = manifestJson["gameVersion"].Value<string>();
+        //            GameVersion = string.IsNullOrEmpty(gameVersion) ? "E.R.R" : gameVersion;
+        //        }
+
+        //        var assemblyText = File.ReadAllText(assemblyFile);
+        //        var assemblyVersion = assemblyVersionRegex.Match(assemblyText).Groups[1].Value;
+        //        if (!assemblyVersion.Equals(PluginVersion))
+        //        {
+        //            Logger.LogWarning("PluginVersion {0} does not match AssemblyVersion {1}", PluginVersion, assemblyVersion);
+        //        }
+
+
+        //        return;// true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw;
+        //        Log.LogErrorFromException(ex);
+        //        return;// false;
+        //    }
+        //}
     }
 }
