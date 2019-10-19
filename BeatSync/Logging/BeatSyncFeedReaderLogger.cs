1b94d1715d6c23b9dc561f05a4aff96912fb658d
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-10-19 17:29:20 -0500

    If Debug build, prefix SongFeedReader log messages to set them apart.

--- BeatSync/Logging/BeatSyncFeedReaderLogger.cs
+++ BeatSync/Logging/BeatSyncFeedReaderLogger.cs
@@ -6,6 +6,11 @@ namespace BeatSync.Logging
 {
     public class BeatSyncFeedReaderLogger : FeedReaderLoggerBase
     {
+#if DEBUG
+        private const string MessagePrefix = "-SongFeedReaders-: ";
+#else
+        private const string MessagePrefix = "";
+#endif
         private BeatSyncFeedReaderLogger()
         {
             LoggerName = "BeatSync";
@@ -16,47 +21,48 @@ namespace BeatSync.Logging
         {
             LogController = controller;
         }
+
         public override void Debug(string message, [CallerFilePath] string file = "", [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
         {
             if (LogLevel > SongFeedReaders.Logging.LogLevel.Debug)
                 return;
-            Logger.log?.Debug(message);
+            Logger.log?.Debug(MessagePrefix + message);
         }
 
         public override void Error(string message, [CallerFilePath] string file = "", [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
         {
             if (LogLevel > SongFeedReaders.Logging.LogLevel.Error)
                 return;
-            Logger.log?.Error(message);
+            Logger.log?.Error(MessagePrefix + message);
         }
 
         public override void Exception(string message, Exception e, [CallerFilePath] string file = "", [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
         {
             if (LogLevel > SongFeedReaders.Logging.LogLevel.Exception)
                 return;
-            Logger.log?.Error($"{message}: {e.Message}");
+            Logger.log?.Error($"{MessagePrefix + message}: {e.Message}");
             Logger.log?.Debug(e);
         }
-
+        
         public override void Info(string message, [CallerFilePath] string file = "", [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
         {
             if (LogLevel > SongFeedReaders.Logging.LogLevel.Info)
                 return;
-            Logger.log?.Info(message);
+            Logger.log?.Info(MessagePrefix + message);
         }
 
         public override void Trace(string message, [CallerFilePath] string file = "", [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
         {
             if (LogLevel > SongFeedReaders.Logging.LogLevel.Trace)
                 return;
-            Logger.log?.Debug(message);
+            Logger.log?.Debug(MessagePrefix + message);
         }
 
         public override void Warning(string message, [CallerFilePath] string file = "", [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
         {
             if (LogLevel > SongFeedReaders.Logging.LogLevel.Warning)
                 return;
-            Logger.log?.Warn(message);
+            Logger.log?.Warn(MessagePrefix + message);
         }
     }
 }
