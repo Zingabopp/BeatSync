697c285bebd73cad2b0966e8c1c136eba8ae56e5
Zingabopp <zingabopp@gmail.com>
2020-01-16 20:11:11 -0600

    Added ISongTarget interface

---
+++ BeatSyncLib/Downloader/ISongTarget.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeatSyncLib.Downloader
+{
+    public interface ISongTarget
+    {
+        string TargetName { get; }
+
+
+
+
+
+    }
+    public enum TargetSource
+    {
+        None = 0,
+        Zip = 1 << 0,
+        Directory = 1 << 1
+    }
+}
