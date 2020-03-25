d18fede1bb1b6edcfd7de23a0e18758d8f765d4b
Zingabopp <Zingabopp@gmail.com>
2020-03-24 23:17:50 -0500

    BeatSyncConsole progress

--- BeatSyncConsole/Program.cs
+++ BeatSyncConsole/Program.cs
@@ -1,7 +1,12 @@
-﻿using BeatSyncLib.Downloader;
+﻿using BeatSyncConsole.Configs;
+using BeatSyncLib.Downloader;
 using BeatSyncLib.Downloader.Targets;
 using BeatSyncLib.History;
+using BeatSyncLib.Playlists;
+using Newtonsoft.Json;
 using SongFeedReaders.Data;
+using SongFeedReaders.Readers;
+using SongFeedReaders.Readers.BeatSaver;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,6 +18,14 @@ namespace BeatSyncConsole
 {
     class Program
     {
+        internal static readonly string ConfigPath = "config.json";
+        internal static readonly string HistoryPath = "history.json";
+        internal static readonly string ConfigBackupPath = "config.json.bak";
+
+        internal static HistoryManager HistoryManager;
+        internal static DownloadManager manager = new DownloadManager(3);
+        internal static IJobBuilder JobBuilder;
+        internal static Config Config;
         public static IJobBuilder CreateJobBuilder()
         {
             string tempDirectory = "Temp";
@@ -30,6 +43,7 @@ namespace BeatSyncConsole
             {
                 HistoryEntry entry = c.CreateHistoryEntry();
                 // Add entry to history.
+                HistoryManager.TryAdd(c.Song.Hash, entry);
                 if (c.Successful)
                 {
                     // Add song to playlist.
@@ -47,25 +61,15 @@ namespace BeatSyncConsole
 
         }
 
-        static async Task Main(string[] args)
+        public static async Task DownloadSongsAsync(IEnumerable<ISong> songs)
         {
-            SongFeedReaders.WebUtils.Initialize(new WebUtilities.WebWrapper.WebClientWrapper());
-            List<ISong> songs = new List<ISong>();
-            ISong song = new ScrapedSong("19f2879d11a91b51a5c090d63471c3e8d9b7aee3", "Believer", "Rustic", "b");
-            songs.Add(song);
-            songs.Add(new ScrapedSong("fb557e1746a49376473d7d40159aa6290d4a9b66", "ruckyshit", "ruckus", "7ef5"));
-            songs.Add(new ScrapedSong("68496811309fe62303edde686eb160f8e45aa9ce", "The Thrill (Porter Robinson Remix)", "ruckus", "4a4c"));
-            songs.Add(new ScrapedSong("b44d11fc21debe3f227b582e22e200095ba82aa7", "Silver Moon", "ruckus", "650"));
-            //songs.Add(new ScrapedSong("660e2e200b317460ef1a64b66347d54dc4df396e", "Ursa Minor |Electron Mix|", "ruckus", "bcc"));
-            DownloadManager manager = new DownloadManager(3);
             CancellationTokenSource cts = new CancellationTokenSource();
             manager.Start(cts.Token);
-            IJobBuilder jobBuilder = CreateJobBuilder();
             HashSet<IJob> runningJobs = new HashSet<IJob>();
             int completedJobs = 0;
             foreach (ISong songToAdd in songs)
             {
-                IJob job = jobBuilder.CreateJob(songToAdd);
+                IJob job = JobBuilder.CreateJob(songToAdd);
                 job.JobProgressChanged += (s, p) =>
                 {
                     IJob j = (IJob)s;
@@ -95,31 +99,144 @@ namespace BeatSyncConsole
             {
                 Console.WriteLine(ex);
             }
-            Console.WriteLine("Press any key to continue...");
-            Console.Read();
         }
 
-        public async Task Thing()
+
+        static async Task InitializeConfigAsync()
         {
-            IJobBuilder jobBuilder = null;
-            int c;
-            JobFinishedAsyncCallback jobFinishedCallback = new JobFinishedAsyncCallback(async c =>
+            try
             {
-                await Task.Delay(500);
-            });
-            Func<int, Task> callback = async (c) =>
+                if (File.Exists(ConfigPath))
+                {
+                    Config = JsonConvert.DeserializeObject<Config>(await File.ReadAllTextAsync(ConfigPath).ConfigureAwait(false));
+                }
+            }
+            catch (Exception ex)
             {
+                Console.WriteLine("Invalid config.json file, using defaults.");
+            }
+            if (Config == null)
+            {
+                Config = new Config();
+            }
+            Config.FillDefaults();
+            if (Config.ConfigChanged)
+            {
+                try
+                {
+                    if (File.Exists(ConfigPath))
+                        File.Copy(ConfigPath, ConfigBackupPath);
+                    await File.WriteAllTextAsync(ConfigPath, JsonConvert.SerializeObject(Config, Formatting.Indented)).ConfigureAwait(false);
+                    if (File.Exists(ConfigBackupPath))
+                        File.Delete(ConfigBackupPath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error updating config file.");
+                    Console.WriteLine(ex);
+                }
+            }
+        }
 
-                await Task.Delay(500);
+        static async Task GetBeatSaverAsync()
+        {
+            var config = Config.BeatSyncConfig.BeatSaver;
+            var reader = new SongFeedReaders.Readers.BeatSaver.BeatSaverReader(config.MaxConcurrentPageChecks);
+            List<IPlaylist> playlists = new List<IPlaylist>();
+            if (config.FavoriteMappers.Enabled)
+            {
+                var favoriteMappersSettings = (BeatSaverFeedSettings)config.FavoriteMappers.ToFeedSettings();
+                //ProcessResults(await reader.GetSongsFromFeedAsync(config.FavoriteMappers.ToFeedSettings()).ConfigureAwait(false));
+            }
+            if (config.Hot.Enabled)
+            {
+                playlists.Clear();
+                if (Config.BeatSyncConfig.AllBeatSyncSongsPlaylist)
+                    playlists.Add(PlaylistManager.GetPlaylist(BuiltInPlaylist.BeatSyncAll));
+                if (config.Hot.CreatePlaylist)
+                    playlists.Add(PlaylistManager.GetPlaylist(BuiltInPlaylist.BeatSaverHot));
+                ProcessResults(await reader.GetSongsFromFeedAsync(config.Hot.ToFeedSettings()).ConfigureAwait(false), playlists);
+            }
+            if (config.Downloads.Enabled)
+            {
+                playlists.Clear();
+                if (Config.BeatSyncConfig.AllBeatSyncSongsPlaylist)
+                    playlists.Add(PlaylistManager.GetPlaylist(BuiltInPlaylist.BeatSyncAll));
+                if (config.Downloads.CreatePlaylist)
+                    playlists.Add(PlaylistManager.GetPlaylist(BuiltInPlaylist.BeatSaverDownloads));
+                ProcessResults(await reader.GetSongsFromFeedAsync(config.Downloads.ToFeedSettings()).ConfigureAwait(false), playlists);
+            }
+        }
+
+        public static void ProcessResults(FeedResult feedResult, IEnumerable<IPlaylist> playlists)
+        {
+            if (!feedResult.Successful)
                 return;
-            };
-
-            jobBuilder.SetDefaultJobFinishedAsyncCallback(async c =>
+            if(feedResult.Songs.Count == 0)
             {
-                await Task.Delay(500);
-            });
+                Console.WriteLine("No songs");
+                return;
+            }
+            if (playlists == null)
+                playlists = Array.Empty<IPlaylist>();
+            void JobFinishedCallback(object sender, JobResult jobResult)
+            {
+                if (jobResult.Successful)
+                {
+                    var song = jobResult.Song;
+                    Console.WriteLine($"Downloaded {song.ToString()} successfully.");
+                    foreach (var playlist in playlists)
+                    {
+                        playlist.TryAdd(song.Hash, song.Name, song.Key, song.LevelAuthorName);
+                    }
+                }
+                else
+                    Console.WriteLine($"Failed to download {jobResult.Song.ToString()}.");
+            }
+            foreach (var song in feedResult.Songs.Values)
+            {
+                if(HistoryManager.TryGetValue(song.Hash, out HistoryEntry existing))
+                {
+                    if (!existing.AllowRetry)
+                    {
+                        Console.WriteLine($"Skipping song: {song}");
+                        if(existing.Flag == HistoryFlag.Downloaded)
+                        {
+                            foreach (var playlist in playlists)
+                            {
+                                playlist.TryAdd(song.Hash, song.Name, song.Key, song.LevelAuthorName);
+                            }
+                        }
+                        continue;
+                    }
+                }
+                var newJob = JobBuilder.CreateJob(song);
+                manager.TryPostJob(newJob, out IJob postedJob);
+                postedJob.JobFinished += JobFinishedCallback; // TODO: Race condition here, might run callback twice.
+                if(postedJob.JobState == JobState.Finished)
+                {
+                    postedJob.JobFinished -= JobFinishedCallback;
+                    JobFinishedCallback(postedJob, postedJob.Result);
+                }
+            }
+        }
 
+        static async Task Main(string[] args)
+        {
+            await InitializeConfigAsync().ConfigureAwait(false);
+            SongFeedReaders.WebUtils.Initialize(new WebUtilities.WebWrapper.WebClientWrapper());
+            manager = new DownloadManager(Config.BeatSyncConfig.MaxConcurrentDownloads);
+            HistoryManager = new HistoryManager(HistoryPath);
+            HistoryManager.Initialize();
+            manager.Start(CancellationToken.None);
+            JobBuilder = CreateJobBuilder();
+            await GetBeatSaverAsync().ConfigureAwait(false);
 
+            await manager.CompleteAsync().ConfigureAwait(false);
+            PlaylistManager.WriteAllPlaylists();
+            HistoryManager.WriteToFile();
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
         }
     }
 }
