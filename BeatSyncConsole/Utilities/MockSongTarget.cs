88474faf2e21d921787365177a79c0e2ea7b95de
Zingabopp <Zingabopp@gmail.com>
2020-05-30 22:36:30 -0500

    MockSongTarget for testing

---
+++ BeatSyncConsole/Utilities/MockSongTarget.cs
@@ -0,0 +1,22 @@
+﻿using BeatSyncLib.Downloader.Targets;
+using SongFeedReaders.Data;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeatSyncConsole.Utilities
+{
+    public class MockSongTarget : SongTarget
+    {
+        protected static Task<SongState> State_NotWanted = Task.FromResult(SongState.NotWanted);
+        public override string TargetName => "MockSongTarget";
+
+        public override Task<SongState> CheckSongExistsAsync(string songHash) => State_NotWanted;
+
+        public override Task<TargetResult> TransferAsync(ISong song, Stream sourceStream, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
