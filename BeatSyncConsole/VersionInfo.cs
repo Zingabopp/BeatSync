fe818a8c55ffc6425e8479b95e2123c7ffa949d7
Zingabopp <Zingabopp@gmail.com>
2020-08-05 23:59:01 -0500

    CI version UserAgent fix

--- BeatSyncConsole/VersionInfo.cs
+++ BeatSyncConsole/VersionInfo.cs
@@ -8,18 +8,23 @@ namespace BeatSyncConsole
 
         public static string Description
         {
-            get 
-            { 
-                if(_versionDescription == null)
+            get
+            {
+                if (_versionDescription == null)
                 {
                     string? versionDescription = Assembly.GetExecutingAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+                    versionDescription = "Official-master-cfa3d9c";
                     if (versionDescription != null && versionDescription.Length > 0)
+                    {
+                        if (versionDescription.Contains('+'))
+                            versionDescription = versionDescription.Substring(0, versionDescription.IndexOf('+'));
                         versionDescription = $" {versionDescription}";
+                    }
                     else
                         versionDescription = " Unknown Build";
                     _versionDescription = versionDescription;
                 }
-                return _versionDescription; 
+                return _versionDescription;
             }
         }
 
