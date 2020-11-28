038d7216e00863be934d128764bf990427ebda99
Zingabopp <Zingabopp@gmail.com>
2020-11-28 12:18:09 -0600

    Option to check playlists for missing songs and download

---
+++ BeatSyncLib/Configs/PlaylistsFeedConfigs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeatSaberPlaylistsLib;
+using SongFeedReaders.Readers;
+using SongFeedReaders.Readers.BeastSaber;
+using BeatSyncLib.Playlists;
+
+namespace BeatSyncLib.Configs
+{
+    public class PlaylistFeeds : FeedConfigBase
+    {
+        #region Defaults
+        protected override bool DefaultEnabled => false;
+
+        protected override int DefaultMaxSongs => 0;
+
+        protected override bool DefaultCreatePlaylist => false;
+
+        protected override PlaylistStyle DefaultPlaylistStyle => PlaylistStyle.Append;
+
+        protected override BuiltInPlaylist DefaultFeedPlaylist => BuiltInPlaylist.BeastSaberFollows;
+        #endregion
+
+        public override IFeedSettings ToFeedSettings()
+        {
+            return new BeastSaberFeedSettings((int)BeastSaberFeedName.Following)
+            {
+                MaxSongs = this.MaxSongs
+            };
+        }
+    }
+}
